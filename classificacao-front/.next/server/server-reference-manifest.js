8d6833eab76752b350418d83c2547b2e7938821f
Sophia <sophialimasantos4@gmail.com>
2025-08-21 16:44:14 -0300

    feat: atualizando branch com a develop

--- classificacao-front/.next/server/server-reference-manifest.js
+++ classificacao-front/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"5274XIhAT9sqPPVRdJ+uV3FMmN/NFjqEwIulmb2luqQ=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"IkJP8gzoI4oceTff/TOeJn5e1LqCQs5LYV3+PPyfsVs=\"\n}"
