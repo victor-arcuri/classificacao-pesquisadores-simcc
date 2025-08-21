84cdc2e84298968c9caa7f3a928d29ecfc2c08b4
Victor Romano Arcuri <victor.r.arcuri@gmail.com>
2025-08-21 14:41:51 -0300

    build: adiciona a estrutura inicial básica da api e a dockeriza

---
+++ api/@types/global.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+        FRONT_URL: string
+        API_PORT: string
+    }
+}
---
+++ api/config/expressConfig.ts
@@ -0,0 +1,21 @@
+import express, { Express } from 'express';
+import cors, { CorsOptions } from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const app: Express = express();
+
+
+const options: CorsOptions = {
+    credentials: true,
+    origin: process.env.FRONT_URL
+};
+
+app.use(cors(options));
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
