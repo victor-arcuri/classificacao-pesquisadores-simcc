5e37f6ffc8c58f4b179aa7e23b1b44e264835f52
Victor Romano Arcuri <victor.r.arcuri@gmail.com>
2025-08-23 19:15:30 -0300

    feat: adiciona a funcionalidade de pesquisa

--- classificacao-front/src/app/page.tsx
+++ classificacao-front/src/app/page.tsx
@@ -1,16 +1,46 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { Poppins } from "next/font/google";
+import { TagResult } from '../types/api'
 
-const poppinsStrong = Poppins({
-  weight: "700",
-  subsets: ["latin"],
-});
-
-const poppinsWeak = Poppins({
-  weight: "500",
-  subsets: ["latin"],
-});
+const poppinsStrong = Poppins({ weight: "700", subsets: ["latin"] });
+const poppinsWeak = Poppins({ weight: "500", subsets: ["latin"] });
 
 export default function Home() {
+  const [query, setQuery] = useState("");
+  const [categories, setCategories] = useState<TagResult[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (!query) {
+      setCategories([]);
+      return;
+    }
+
+    const handler = setTimeout(async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(`http://localhost:5000/api/search?search=${encodeURIComponent(query)}`);
+
+      if (!res.ok) {
+        console.debug("Erro HTTP:", res.status, res.statusText);
+        return;
+      }
+
+      const data = await res.json();
+      console.log(data);
+      setCategories(data.results ?? []);
+    } catch (err) {
+      console.debug("Erro ao buscar categorias", err);
+    } finally {
+      setLoading(false);
+    }
+    }, 500);
+
+    return () => clearTimeout(handler);
+  }, [query]);
+
   return (
     <div className="bg-neutral-100 h-screen">
       <main className="h-full flex justify-center items-center">
@@ -18,35 +48,45 @@ export default function Home() {
           <div className="w-full h-full border rounded-lg border-neutral-300 bg-neutral-50 flex flex-col">
             <div className="mt-14 w-full px-32 h-56 flex flex-col justify-center items-center gap-10">
               <div className="w-full flex justify-center items-center">
-                <p className={`${poppinsStrong.className} text-neutral-800 text-center text-6xl`}>
+                <p
+                  className={`${poppinsStrong.className} text-neutral-800 text-center text-6xl`}
+                >
                   Classificação de Pesquisadores
                 </p>
               </div>
               <div className="w-full flex flex-col gap-2">
-                {/* Div que contém a caixa de busca */}
-                <div className="h-14 rounded-lg border border-neutral-300 w-full shadow"></div>
-                {/* Contêiner dos botões: Adicione 'flex' e 'gap-2' para espaçamento */}
-                <div className="h-8 w-full flex gap-2">
-                  <div className="h-full w-max px-3 bg-blue-400 border rounded-lg flex justify-center items-center">
-                    <p className={`text-neutral-50 text-base ${poppinsWeak.className}`}>
-                      Inteligência Artificial
-                    </p>
-                  </div>
-                  <div className="h-full w-max px-3 bg-blue-400 border rounded-lg flex justify-center items-center">
-                    <p className={`text-neutral-50 text-base ${poppinsWeak.className}`}>
-                      Inteligência Emocional
-                    </p>
-                  </div>
+                <div className="h-14 rounded-lg w-full shadow">
+                  <input
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    className="w-full h-full rounded-lg border border-neutral-300 placeholder-neutral-400 focus:outline-none focus:border-blue-600 text-neutral-800 px-4"
+                    placeholder="Digite alguma categoria para iniciar a busca"
+                  />
                 </div>
-              </div> 
+                <div className="h-8 w-full flex gap-2 overflow-x-auto">
+                  {!loading &&
+                    categories.slice(0, 5).map((cat, i) => (
+                      <div
+                        key={i}
+                        className="h-full w-max px-3 bg-blue-400 border rounded-lg flex justify-center items-center"
+                      >
+                        <p
+                          className={`text-neutral-50 text-base ${poppinsWeak.className}`}
+                        >
+                          {cat.name}
+                        </p>
+                      </div>
+                    ))}
+                </div>
+              </div>
             </div>
             <div className="w-full h-full flex-1 pb-10 px-10 flex flex-col">
-              <p className={`${poppinsWeak.className} text-neutral-800 text-2xl mb-4`}>
+              <p
+                className={`${poppinsWeak.className} text-neutral-800 text-2xl mb-4`}
+              >
                 Pesquisadores
               </p>
-              <div className="w-full border rounded-lg border-neutral-300 flex-1">
-                
-              </div>
+              <div className="w-full border rounded-lg border-neutral-300 flex-1"></div>
             </div>
           </div>
         </div>
