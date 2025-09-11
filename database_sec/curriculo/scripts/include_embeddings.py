55aff0ab39fcf34c5308ab54a9d6c9a71e76df1c
Sophia Lima <sophialimasantos4@gmail.com>
2025-09-10 21:53:06 -0300

    feat: corrigindo e adicionando as colunas em include_embeddings

--- database_sec/curriculo/scripts/include_embeddings.py
+++ database_sec/curriculo/scripts/include_embeddings.py
@@ -5,33 +5,62 @@ from dotenv import load_dotenv
 import numpy as np
 
 def criar_conexao_e_inserir_dados(df_embeddings_curriculos):
+    """
+    Cria uma conexão com o banco de dados e insere ou atualiza os embeddings de currículos.
+    """
     try:
         load_dotenv()
 
         usuario = os.getenv('CURRICULO_DB_USER')
         senha = os.getenv('CURRICULO_DB_PASSWORD')
-        host = 'localhost' 
+        host = 'localhost'
         porta = os.getenv('CURRICULO_DB_PORT_HOST')
         banco = os.getenv('CURRICULO_DB_NAME')
         
         connection_string = f'postgresql+psycopg2://{usuario}:{senha}@{host}:{porta}/{banco}'
         engine = create_engine(connection_string)
 
+        # Query SQL corrigida para todas as colunas
         sql = text("""
-            INSERT INTO curriculos (id_pesquisador, long_embeddings, short_embeddings)
-            VALUES (:id, :emb_p, :emb_l)
+            INSERT INTO curriculos (
+                id_pesquisador, abstract_embeddings, articles_embeddings, 
+                project_name_embeddings, description_project_embeddings, 
+                great_area_embeddings, area_specialty_embeddings, 
+                patent_embeddings, book_chapter_embeddings, event_name_embeddings
+            )
+            VALUES (
+                :id_pesquisador, :abstract_embeddings, :articles_embeddings, 
+                :project_name_embeddings, :description_project_embeddings, 
+                :great_area_embeddings, :area_specialty_embeddings, 
+                :patent_embeddings, :book_chapter_embeddings, :event_name_embeddings
+            )
             ON CONFLICT (id_pesquisador) DO UPDATE SET
-                long_embeddings = EXCLUDED.long_embeddings,
-                short_embeddings = EXCLUDED.short_embeddings;
+                abstract_embeddings = EXCLUDED.abstract_embeddings,
+                articles_embeddings = EXCLUDED.articles_embeddings,
+                project_name_embeddings = EXCLUDED.project_name_embeddings,
+                description_project_embeddings = EXCLUDED.description_project_embeddings,
+                great_area_embeddings = EXCLUDED.great_area_embeddings,
+                area_specialty_embeddings = EXCLUDED.area_specialty_embeddings,
+                patent_embeddings = EXCLUDED.patent_embeddings,
+                book_chapter_embeddings = EXCLUDED.book_chapter_embeddings,
+                event_name_embeddings = EXCLUDED.event_name_embeddings;
         """)
 
         with engine.begin() as conn:
             for row in df_embeddings_curriculos.itertuples():
-                conn.execute(sql, {
-                    "id": row.id_pesquisador,
-                    "emb_p": row.long_embeddings,
-                    "emb_l": row.short_embeddings
-                })
+                parametros = {
+                    "id_pesquisador": row.id_pesquisador,
+                    "abstract_embeddings": row.abstract_embeddings,
+                    "articles_embeddings": row.articles_embeddings,
+                    "project_name_embeddings": row.project_name_embeddings,
+                    "description_project_embeddings": row.description_project_embeddings,
+                    "great_area_embeddings": row.great_area_embeddings,
+                    "area_specialty_embeddings": row.area_specialty_embeddings,
+                    "patent_embeddings": row.patent_embeddings,
+                    "book_chapter_embeddings": row.book_chapter_embeddings,
+                    "event_name_embeddings": row.event_name_embeddings
+                }
+                conn.execute(sql, parametros)
         
         print("Dados de currículos inseridos/atualizados com sucesso!")
 
