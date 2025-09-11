41a226e9816007d4269e822a9bdeedd8bc769325
Victor Romano Arcuri <victor.r.arcuri@gmail.com>
2025-09-11 19:40:42 -0300

    feat: adiciona seed de relacionamentos aleatórios para teste

--- api/config/prismaClient.ts
+++ api/config/prismaClient.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client';
+ import prismaRandom from 'prisma-extension-random';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends(prismaRandom());
 export default prisma;
